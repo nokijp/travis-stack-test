5870b4c97d6c94ecdcec5073ba70e6c0b502a2c4
nokijp <nokijp@users.noreply.github.com>
2016-09-22 12:33:55 +0900

    add a simple stack project

---
+++ src/TestLib.hs
@@ -0,0 +1,4 @@
+module TestLib (testFunc) where
+
+testFunc :: Int -> Int
+testFunc = (+ 1)
---
+++ test/Spec.hs
@@ -0,0 +1,8 @@
+import Test.Hspec
+import TestLib
+
+main :: IO ()
+main = hspec $
+  describe "testFunc" $
+    it "returns 2 when given 1" $
+      testFunc 1 `shouldBe` 2
